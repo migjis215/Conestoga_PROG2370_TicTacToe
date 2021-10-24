427323309678721afab554b54f2440d69f3ca5b6
Jisung <jisungkim215@gmail.com>
2021-10-24 00:40:24 -0400

    init

---
+++ JKimAssignment1/TicTacToe.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace JKimAssignment1
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picThirdRowLeft = new System.Windows.Forms.PictureBox();
+            this.picThirdRowRight = new System.Windows.Forms.PictureBox();
+            this.picThirdRowCenter = new System.Windows.Forms.PictureBox();
+            this.picFirstRowLeft = new System.Windows.Forms.PictureBox();
+            this.picSecondRowLeft = new System.Windows.Forms.PictureBox();
+            this.picFirstRowRight = new System.Windows.Forms.PictureBox();
+            this.picSecondRowRight = new System.Windows.Forms.PictureBox();
+            this.picFirstRowCenter = new System.Windows.Forms.PictureBox();
+            this.picSecondRowCenter = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picThirdRowLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picThirdRowRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picThirdRowCenter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFirstRowLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSecondRowLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFirstRowRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSecondRowRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFirstRowCenter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSecondRowCenter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picThirdRowLeft
+            // 
+            this.picThirdRowLeft.BackColor = System.Drawing.Color.Transparent;
+            this.picThirdRowLeft.Location = new System.Drawing.Point(256, 672);
+            this.picThirdRowLeft.Name = "picThirdRowLeft";
+            this.picThirdRowLeft.Size = new System.Drawing.Size(280, 280);
+            this.picThirdRowLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picThirdRowLeft.TabIndex = 28;
+            this.picThirdRowLeft.TabStop = false;
+            this.picThirdRowLeft.Click += new System.EventHandler(this.setImage);
+            // 
+            // picThirdRowRight
+            // 
+            this.picThirdRowRight.BackColor = System.Drawing.Color.Transparent;
+            this.picThirdRowRight.Location = new System.Drawing.Point(858, 672);
+            this.picThirdRowRight.Name = "picThirdRowRight";
+            this.picThirdRowRight.Size = new System.Drawing.Size(280, 280);
+            this.picThirdRowRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picThirdRowRight.TabIndex = 29;
+            this.picThirdRowRight.TabStop = false;
+            this.picThirdRowRight.Click += new System.EventHandler(this.setImage);
+            // 
+            // picThirdRowCenter
+            // 
+            this.picThirdRowCenter.BackColor = System.Drawing.Color.Transparent;
+            this.picThirdRowCenter.Location = new System.Drawing.Point(556, 672);
+            this.picThirdRowCenter.Name = "picThirdRowCenter";
+            this.picThirdRowCenter.Size = new System.Drawing.Size(280, 280);
+            this.picThirdRowCenter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picThirdRowCenter.TabIndex = 30;
+            this.picThirdRowCenter.TabStop = false;
+            this.picThirdRowCenter.Click += new System.EventHandler(this.setImage);
+            // 
+            // picFirstRowLeft
+            // 
+            this.picFirstRowLeft.BackColor = System.Drawing.Color.Transparent;
+            this.picFirstRowLeft.Location = new System.Drawing.Point(256, 88);
+            this.picFirstRowLeft.Name = "picFirstRowLeft";
+            this.picFirstRowLeft.Size = new System.Drawing.Size(280, 280);
+            this.picFirstRowLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picFirstRowLeft.TabIndex = 31;
+            this.picFirstRowLeft.TabStop = false;
+            this.picFirstRowLeft.Click += new System.EventHandler(this.setImage);
+            // 
+            // picSecondRowLeft
+            // 
+            this.picSecondRowLeft.BackColor = System.Drawing.Color.Transparent;
+            this.picSecondRowLeft.Location = new System.Drawing.Point(256, 380);
+            this.picSecondRowLeft.Name = "picSecondRowLeft";
+            this.picSecondRowLeft.Size = new System.Drawing.Size(280, 280);
+            this.picSecondRowLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picSecondRowLeft.TabIndex = 32;
+            this.picSecondRowLeft.TabStop = false;
+            this.picSecondRowLeft.Click += new System.EventHandler(this.setImage);
+            // 
+            // picFirstRowRight
+            // 
+            this.picFirstRowRight.BackColor = System.Drawing.Color.Transparent;
+            this.picFirstRowRight.Location = new System.Drawing.Point(858, 88);
+            this.picFirstRowRight.Name = "picFirstRowRight";
+            this.picFirstRowRight.Size = new System.Drawing.Size(280, 280);
+            this.picFirstRowRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picFirstRowRight.TabIndex = 33;
+            this.picFirstRowRight.TabStop = false;
+            this.picFirstRowRight.Click += new System.EventHandler(this.setImage);
+            // 
+            // picSecondRowRight
+            // 
+            this.picSecondRowRight.BackColor = System.Drawing.Color.Transparent;
+            this.picSecondRowRight.Location = new System.Drawing.Point(858, 380);
+            this.picSecondRowRight.Name = "picSecondRowRight";
+            this.picSecondRowRight.Size = new System.Drawing.Size(280, 280);
+            this.picSecondRowRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picSecondRowRight.TabIndex = 34;
+            this.picSecondRowRight.TabStop = false;
+            this.picSecondRowRight.Click += new System.EventHandler(this.setImage);
+            // 
+            // picFirstRowCenter
+            // 
+            this.picFirstRowCenter.BackColor = System.Drawing.Color.Transparent;
+            this.picFirstRowCenter.Location = new System.Drawing.Point(556, 88);
+            this.picFirstRowCenter.Name = "picFirstRowCenter";
+            this.picFirstRowCenter.Size = new System.Drawing.Size(280, 280);
+            this.picFirstRowCenter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picFirstRowCenter.TabIndex = 35;
+            this.picFirstRowCenter.TabStop = false;
+            this.picFirstRowCenter.Click += new System.EventHandler(this.setImage);
+            // 
+            // picSecondRowCenter
+            // 
+            this.picSecondRowCenter.BackColor = System.Drawing.Color.Transparent;
+            this.picSecondRowCenter.Location = new System.Drawing.Point(556, 380);
+            this.picSecondRowCenter.Name = "picSecondRowCenter";
+            this.picSecondRowCenter.Size = new System.Drawing.Size(280, 280);
+            this.picSecondRowCenter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picSecondRowCenter.TabIndex = 36;
+            this.picSecondRowCenter.TabStop = false;
+            this.picSecondRowCenter.Click += new System.EventHandler(this.setImage);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::JKimAssignment1.Properties.Resources.tic_tac_toe_bg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1399, 1061);
+            this.Controls.Add(this.picThirdRowLeft);
+            this.Controls.Add(this.picThirdRowRight);
+            this.Controls.Add(this.picThirdRowCenter);
+            this.Controls.Add(this.picFirstRowLeft);
+            this.Controls.Add(this.picSecondRowLeft);
+            this.Controls.Add(this.picFirstRowRight);
+            this.Controls.Add(this.picSecondRowRight);
+            this.Controls.Add(this.picFirstRowCenter);
+            this.Controls.Add(this.picSecondRowCenter);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "TicTacToe";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.TicTacToe_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picThirdRowLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picThirdRowRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picThirdRowCenter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFirstRowLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSecondRowLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFirstRowRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSecondRowRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFirstRowCenter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSecondRowCenter)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picThirdRowLeft;
+        private System.Windows.Forms.PictureBox picThirdRowRight;
+        private System.Windows.Forms.PictureBox picThirdRowCenter;
+        private System.Windows.Forms.PictureBox picFirstRowLeft;
+        private System.Windows.Forms.PictureBox picSecondRowLeft;
+        private System.Windows.Forms.PictureBox picFirstRowRight;
+        private System.Windows.Forms.PictureBox picSecondRowRight;
+        private System.Windows.Forms.PictureBox picFirstRowCenter;
+        private System.Windows.Forms.PictureBox picSecondRowCenter;
+    }
+}
+
---
+++ JKimAssignment1/TicTacToe.cs
@@ -0,0 +1,303 @@
+﻿/* TicTacToe.cs
+ * Assignment 1
+ * Revision History
+ *      Jisung Kim, 2021.09.28: Created
+ *      Jisung Kim, 2021.10.01: Modified code
+ *      
+ */
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace JKimAssignment1
+{
+    /// <summary>
+    /// A class to use images to play Tic-Tac-Toe game
+    /// </summary>
+    public partial class TicTacToe : Form
+    {
+        // Declaring class variables and constants
+        private const int SIZE = 3;
+
+        private readonly Image X = Properties.Resources.x;
+        private readonly Image O = Properties.Resources.o;
+
+        private Image[,] marks;
+
+        private bool isPlaying;
+        private bool isTurnOfX;
+        private int numberOfMarks;
+
+        /// <summary>
+        /// Default constructor of the TicTacToe class
+        /// </summary>
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        private void TicTacToe_Load(object sender, EventArgs e)
+        {
+            initialize();
+        }
+
+        /// <summary>
+        /// Initialize variables for game start or restart
+        /// </summary>
+        private void initialize()
+        {
+            marks = new Image[SIZE, SIZE];
+
+            isPlaying = true;
+            isTurnOfX = true;
+            numberOfMarks = 0;
+
+            foreach (var control in this.Controls)
+            {
+                if (control is PictureBox pictureBox)
+                {
+                    pictureBox.Image = null;
+                }
+            }
+        }
+
+        // Click event handler that set the image in the selected picture box
+        private void setImage(object sender, EventArgs e)
+        {
+            if (sender is PictureBox pictureBox)
+            {
+                if (pictureBox.Image != null)
+                {
+                    return;
+                }
+
+                if (isTurnOfX)
+                {
+                    pictureBox.Image = X;
+                    isTurnOfX = false;
+                }
+                else
+                {
+                    pictureBox.Image = O;
+                    isTurnOfX = true;
+                }
+
+                addImageToArray(pictureBox);
+
+                numberOfMarks++;
+                
+                displayResults(checkGameEnded());
+            }
+        }
+
+        /// <summary>
+        /// Add the image set in the picture box to the array
+        /// </summary>
+        /// <param name="pictureBox">The picture box clicked by user</param>
+        private void addImageToArray(PictureBox pictureBox)
+        {
+            switch (pictureBox.Name)
+            {
+                case "picFirstRowLeft":
+                    marks[0, 0] = pictureBox.Image;
+                    break;
+                case "picFirstRowCenter":
+                    marks[0, 1] = pictureBox.Image;
+                    break;
+                case "picFirstRowRight":
+                    marks[0, 2] = pictureBox.Image;
+                    break;
+                case "picSecondRowLeft":
+                    marks[1, 0] = pictureBox.Image;
+                    break;
+                case "picSecondRowCenter":
+                    marks[1, 1] = pictureBox.Image;
+                    break;
+                case "picSecondRowRight":
+                    marks[1, 2] = pictureBox.Image;
+                    break;
+                case "picThirdRowLeft":
+                    marks[2, 0] = pictureBox.Image;
+                    break;
+                case "picThirdRowCenter":
+                    marks[2, 1] = pictureBox.Image;
+                    break;
+                case "picThirdRowRight":
+                    marks[2, 2] = pictureBox.Image;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Check if the game is ended
+        /// </summary>
+        /// <returns>The image of the winner or null</returns>
+        private Image checkGameEnded()
+        {
+            // The winner can be determined after at least 5 turns
+            if (numberOfMarks < 5)
+            {
+                return null;
+            }
+
+            isPlaying = true;
+            Image previousMark = null;
+
+            // Check all the rows
+            for (int row = 0; row < SIZE; row++)
+            {
+                previousMark = null;
+                for (int column = 0; column < SIZE; column++)
+                {
+                    if (marks[row, column] == null)
+                    {
+                        break;
+                    }
+
+                    if (previousMark == null)
+                    {
+                        previousMark = marks[row, column];
+                    }
+                    else if (previousMark != marks[row, column])
+                    {
+                        break;
+                    }
+
+                    if (isLastIndex(column))
+                    {
+                        isPlaying = false;
+                        return previousMark;
+                    }
+                }
+            }
+
+            // Check all the columns
+            for (int row = 0; row < SIZE; row++)
+            {
+                previousMark = null;
+                for (int column = 0; column < SIZE; column++)
+                {
+                    if (marks[column, row] == null)
+                    {
+                        break;
+                    }
+
+                    if (previousMark == null)
+                    {
+                        previousMark = marks[column, row];
+                    }
+                    else if (previousMark != marks[column, row])
+                    {
+                        break;
+                    }
+
+                    if (isLastIndex(column))
+                    {
+                        isPlaying = false;
+                        return previousMark;
+                    }
+                }
+            }
+
+            // Check all the diagonal lines
+            previousMark = null;
+            for (int row = 0; row < SIZE; row++)
+            {
+                if (marks[row, row] == null)
+                {
+                    break;
+                }
+
+                if (previousMark == null)
+                {
+                    previousMark = marks[row, row];
+                }
+                else if (previousMark != marks[row, row])
+                {
+                    break;
+                }
+
+                if (isLastIndex(row))
+                {
+                    isPlaying = false;
+                    return previousMark;
+                }
+            }
+            previousMark = null;
+            for (int row = 0; row < SIZE; row++)
+            {
+                if (marks[row, Math.Abs(row - 2)] == null)
+                {
+                    break;
+                }
+
+                if (previousMark == null)
+                {
+                    previousMark = marks[row, Math.Abs(row - 2)];
+                }
+                else if (previousMark != marks[row, Math.Abs(row - 2)])
+                {
+                    break;
+                }
+
+                if (isLastIndex(row))
+                {
+                    isPlaying = false;
+                    return previousMark;
+                }
+            }
+
+            // Check the empty space
+            foreach (var mark in marks)
+            {
+                if (mark == null)
+                {
+                    return null;
+                }
+            }
+
+            isPlaying = false;
+            return null;
+        }
+
+        /// <summary>
+        /// Check if it is the last index of a row or column
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>true if the number is the last index; otherwise false</returns>
+        private bool isLastIndex(int index)
+        {
+            return index == (SIZE - 1);
+        }
+
+        /// <summary>
+        /// Show the game result message
+        /// </summary>
+        /// <param name="mark">The image of the winner</param>
+        private void displayResults(Image mark)
+        {
+            if (isPlaying)
+            {
+                return;
+            }
+            
+            if (mark == X)
+            {
+                MessageBox.Show("X Wins");
+            }
+            else if (mark == O)
+            {
+                MessageBox.Show("O Wins");
+            }
+            else
+            {
+                MessageBox.Show("Draw");
+            }
+
+            initialize();
+        }
+    }
+}
